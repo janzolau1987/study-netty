da5409b960423ba0a885e74852c648fa3b6cafb6
janzolau1987 <janzolau1987@gmail.com>
2017-01-10 18:01:35 +0800

    HttpClient与HttpServer测试

---
+++ src/main/java/com/yohotao/study/netty/demo10/HttpServerInboundHandler.java
@@ -0,0 +1,64 @@
+package com.yohotao.study.netty.demo10;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpHeaders.Values;
+import io.netty.handler.codec.http.HttpRequest;
+
+public class HttpServerInboundHandler extends ChannelInboundHandlerAdapter {
+
+    private HttpRequest request;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg)
+            throws Exception {
+        if (msg instanceof HttpRequest) {
+            request = (HttpRequest) msg;
+
+            String uri = request.getUri();
+            System.out.println("Uri:" + uri);
+        }
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+
+            String res = "I am OK";
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
+                    OK, Unpooled.wrappedBuffer(res.getBytes("UTF-8")));
+            response.headers().set(CONTENT_TYPE, "text/plain");
+            response.headers().set(CONTENT_LENGTH,
+                    response.content().readableBytes());
+            if (HttpHeaders.isKeepAlive(request)) {
+                response.headers().set(CONNECTION, Values.KEEP_ALIVE);
+            }
+            ctx.write(response);
+            ctx.flush();
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        System.out.println(cause.getMessage());
+        ctx.close();
+    }
+
+}
